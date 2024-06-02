0387d408abf4b15b6b171c80eb964181f3f6a8d6
Jotham Teshome <jothamteshome@gmail.com>
2024-06-02 17:50:24 -0400

    Added previous job experience and education constants

--- src/constants.js
+++ src/constants.js
@@ -1,20 +1,79 @@
+export const navigationRoutes = [
+    { name: 'Home', route: '/' },
+    { name: 'About', route: '/about' },
+    { name: 'Projects', route: '/projects' },
+    { name: 'Experience', route: '/experience' },
+    { name: 'Contact', route: '/contact' }
+];
+
 export const skills = [
-    { name: "C", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg"},
-    { name: "C++", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg"},
-    { name: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg"},
-    { name: "Bootstrap", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg"},
-    { name: "Dart", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg"},
-    { name: "Docker", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg"},
-    { name: "Flutter", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg"},
-    { name: "GCP", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg"},
-    { name: "Git", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg"},
-    { name: "HTML", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg"},
-    { name: "Javascript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg"},
-    { name: "MySQL", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg"},
+    { name: "C", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" },
+    { name: "C++", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" },
+    { name: "CSS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" },
+    { name: "Bootstrap", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bootstrap/bootstrap-original.svg" },
+    { name: "Dart", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/dart/dart-original.svg" },
+    { name: "Docker", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-plain.svg" },
+    { name: "Flutter", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/flutter/flutter-original.svg" },
+    { name: "GCP", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg" },
+    { name: "Git", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg" },
+    { name: "HTML", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" },
+    { name: "Javascript", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" },
+    { name: "MySQL", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" },
     { name: "Python", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" },
     { name: "Pytorch", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg" },
-    { name: "NextJS", icon : "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg"},
-    { name: "TensorFlow", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg"}
+    { name: "NextJS", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" },
+    { name: "TensorFlow", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg" }
+];
+
+export const experience_items = [
+    {
+        employer: "Michigan State Univerity",
+        start_date: "Aug 2023",
+        end_date: "May 2024",
+        role: "Graduate Teaching Assistant",
+        tasks_completed: ["Evaluated assignments to uphold rigorous academic standards in the Web Application Development course",
+            "Provided students with helpful insight on GitLab by describing the uses for HTML, CSS, and JavaScript in front-end design, including topics such as responsive sizing and dynamic retrieval of data",
+            "Improved students’ understanding of Flask for back-end development by 7% through hosting regularly scheduled office hour sessions"
+        ],
+        logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Michigan_State_University_seal.svg"
+    },
+    {
+        employer: "Michigan State University Federal Credit Union",
+        start_date: "Aug 2022",
+        end_date: "Dec 2022",
+        role: "Software Engineering Intern",
+        tasks_completed: ["Developed a software project, primarily using Flutter, Dart, and the iOS SDK, for a customer by working with a team of six students to enhance customers’ banking experience",
+            "Designed and implemented Digital Transformation of Member Data by working closely with the customer to bring their vision for the product to life"
+        ],
+        logo: "https://assets.bizclikmedia.net/576/9e74408088209589917c0ec30d0f5898:9fddd8f3760b713e18c51d4cde47fa63/404no22rkiiusfca101220200429.jpeg"
+    }
+];
+
+export const education_items = [
+    {
+        institution: "Michigan State University",
+        start_date: "Jan 2023",
+        end_date: "April 2024",
+        degree_aquired: "MS, Computer Science",
+        gpa: "3.85",
+        coursework: ["Natural Language Processing",
+            "Computer Vision",
+            "Pattern Recognition",
+            "Deep Learning",
+            "Algorithmic Graph Theory",
+            "Distributed Systems"
+        ],
+        logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Michigan_State_University_seal.svg"
+    },
+    {
+        institution: "Michigan State University",
+        start_date: "Sep 2019",
+        end_date: "Dec 2022",
+        degree_aquired: "BS, Computer Science",
+        gpa: "3.69",
+        coursework: [],
+        logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Michigan_State_University_seal.svg"
+    }
 ];
 
 export const socialMediaLinks = [
@@ -35,16 +94,4 @@ export const serviceData = [
         name: "Backend Development",
         description: "backend description"
     }
-];
-
-export const navigationRoutes = [
-    { name: 'About', route: '/about' },
-    { name: 'Projects', route: '/projects' },
-    { name: 'Experience', route: '/experience' },
-    { name: 'Contact', route: '/contact' }
-];
-
-export const experiences = [
-    { employer: "Michigan State Univerity", logo: "https://upload.wikimedia.org/wikipedia/en/5/53/Michigan_State_University_seal.svg", start_date: "Aug 2023", end_date: "May 2024", role: "Graduate Teaching Assistant", tasks_completed: []},
-    { employer: "Michigan State University Federal Credit Union", logo: "https://upload.wikimedia.org/wikipedia/commons/2/2c/MSU_Federal_Credit_Union_Logo_%282021%29.svg", start_date: "Aug 2022", end_date: "Dec 2022", role: "Software Engineering Intern", tasks_completed: ["coding"]}
 ];
