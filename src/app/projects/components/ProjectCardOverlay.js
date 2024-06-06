6400db643e2f25f168145633b1652d57412d4db1
Jotham Teshome <jothamteshome@gmail.com>
2024-06-05 22:11:42 -0400

    Separated ProjectsPage elements into separate component files

---
+++ src/app/projects/components/ProjectCardOverlay.js
@@ -0,0 +1,40 @@
+"use client"
+import { projects__page_project_card_overlay_variant } from '@/constants/animationVariants';
+
+import { motion } from 'framer-motion';
+
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import ProjectBadgeRow from './ProjectBadgeRow';
+import ProjectLinksRow from './ProjectLinksRow';
+import Stack from 'react-bootstrap/Stack';
+
+
+/**
+ * Represents the overlay describing details about a project that
+ * appears on hover when the page is large
+ * @param {object} props - The props object 
+ * @param {object} props.project - An object containing information about a give project
+ * @returns {JSX.Element} An card image overlay containing the name, description, and 
+ * additional details of where to find a project
+ */
+const ProjectCardOverlay = function ({ project }) {
+    return (
+        <motion.div
+            variants={projects__page_project_card_overlay_variant}
+            className='d-none d-md-block'>
+            <Card.ImgOverlay className='bg-dark bg-opacity-50'>
+                <Stack direction="horizontal" className='d-flex justify-content-between'>
+                    <Card.Title className='mb-1'>{project.name}</Card.Title>
+                    <ProjectBadgeRow project={project} projectBadgeClassName="mb-2" />
+                </Stack>
+                <Container className='h-100 p-0 pb-4 d-flex flex-column justify-content-between'>
+                    <Card.Text className='text-wrap text-truncate'>{project.description}</Card.Text>
+                    <ProjectLinksRow project={project} />
+                </Container>
+            </Card.ImgOverlay>
+        </motion.div>
+    );
+};
+
+export default ProjectCardOverlay;
---
+++ src/app/projects/components/ProjectLinksRow.js
@@ -0,0 +1,40 @@
+"use client"
+import ProjectDescriptionModal from './ProjectModal';
+import ProjectLink from './ProjectLink';
+import Row from 'react-bootstrap/Row';
+
+
+/**
+ * Represents a row containing links to external sites related to project
+ * @param {object} props - The props object
+ * @param {object} props.project - An object containing information about a project
+ * @returns {JSX.Element} A Row element containing links related to a project if they exist
+ */
+const ProjectLinksRow = function ({ project }) {
+    // Store a list of project links associated with a given project
+    const project_links = [];
+
+    // Check if github link exists for project
+    if (project.links.github) {
+        project_links.push({ link_type: "github", project_name: project.name, link: project.links.github });
+    }
+
+    // Check if live demo link exists for project
+    if (project.links.live_demo) {
+        project_links.push({ link_type: "live_demo", project_name: project.name, link: project.links.live_demo })
+    }
+
+
+    return (
+        <Row className='w-100 mx-auto' xs={1} sm={1} md="auto">
+            {
+                project_links.map((link, i) => (
+                    <ProjectLink key={i} link_info={link} />
+                ))
+            }
+            <ProjectDescriptionModal project={project} />
+        </Row>
+    );
+};
+
+export default ProjectLinksRow;
