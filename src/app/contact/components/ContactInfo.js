5baa56c4a1ea6a4280b2193f72e6c771844acc14
Jotham Teshome <jothamteshome@gmail.com>
2024-06-05 21:40:10 -0400

    Separated ContactPage components into separate files and directory

---
+++ src/app/contact/components/ContactInfo.js
@@ -0,0 +1,38 @@
+"use client"
+import { contact__page_column_variant } from '@/constants/animationVariants';
+
+import { contactInformation } from '@/constants/profileConstants';
+
+import { motion } from 'framer-motion';
+
+import Stack from 'react-bootstrap/Stack';
+import SocialLinks from '@/components/SocialLinks';
+
+
+/**
+ * Represents a stack containing information regarding 
+ * where I am and how to get in contact with me
+ * @returns {JSX.Element} A Stack element containing my email, location, and social links
+ */
+const ContactInfo = function () {
+    return (
+        <motion.div variants={contact__page_column_variant}>
+            <Stack className="mt-3 text-start text-lg-end contact_info" gap={4}>
+                <Stack className='flex-grow-0'>
+                    <h2 className="fw-bold mb-0">Email</h2>
+                    <h5 className="mb-0">{contactInformation.email}</h5>
+                </Stack>
+                <Stack className='flex-grow-0'>
+                    <h2 className="fw-bold mb-0">Location</h2>
+                    <h5 className="mb-0">{contactInformation.location}</h5>
+                </Stack>
+                <Stack className='flex-grow-0'>
+                    <h2 className="fw-bold mb-0">Social</h2>
+                    <SocialLinks />
+                </Stack>
+            </Stack>
+        </motion.div>
+    );
+};
+
+export default ContactInfo;
